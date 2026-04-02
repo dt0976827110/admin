5e99f8b9d3bf6729d1d01bdc9bc2640cecfc59bd
dt0976827110 <dt0976827110@gmail.com>
2026-04-02 21:06:47 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 // ========== config.js ==========
 // ===== API 設定 =====
 const CONFIG = {
@@ -5,7 +6,7 @@ const CONFIG = {
     API_URL: 'https://script.google.com/macros/s/AKfycbzj_V9ywaB7S3CBLinOcWOpxB-9-UM7VnWs7e_v5LtnEbAr9OjAzgdmf0yNg8oiwcbU/exec',
     
     // ⚠️ 請填入你設定的管理密碼
-    PASSWORD: 'aa8888',
+    PASSWORD: 'YOUR_PASSWORD_HERE',
     
     // Sheet ID (已從你的程式碼中取得)
     SHEET_ID: '1K_a9KEizA7zBL9F6Y-DkJCa5QfrRuoTKZdUzLvfYnoo',
@@ -1120,3 +1121,4 @@ const autoreply = {
         }
     }
 };
+
